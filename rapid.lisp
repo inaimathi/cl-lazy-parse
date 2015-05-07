2bc93f2ff20d079fba28ab0df270118de6324587
inaimathi <inaimathi@self>
2015-05-06 22:55:40 -0400

    -Added string buffers to rapids

--- rapid.lisp
+++ rapid.lisp
@@ -1,7 +1,32 @@
 (in-package #:cl-lazy-parse)
 
-;;;;;;;;;; Rapids are streams that don't block
-;; TODO: Make peeking efficient.
+;;;;;;;;;; Trying the buffer approach to "reversibility"
+(defstruct buffer arr size (end-ix 0) (read-ix 0))
+(defun buffer (&key (initial-size 512))
+  (make-buffer :arr (make-string initial-size) :size initial-size))
+
+(defmethod more? ((b buffer)) (> (buffer-end-ix b) (buffer-read-ix b)))
+
+(defmethod place! ((b buffer) (c character))
+  (let ((arr (buffer-arr b)))
+    (when (= (buffer-end-ix b) (length arr))
+      (setf (buffer-arr b) 
+	    (concatenate 'string arr (make-string (length arr))))))
+  (setf (aref (buffer-arr b) (buffer-end-ix b)) c)
+  (incf (buffer-end-ix b))
+  c)
+
+(defmethod read! ((b buffer))
+  (when (more? b)
+    (let ((c (aref (buffer-arr b) (buffer-read-ix b))))
+      (incf (buffer-read-ix b))
+      c)))
+
+(defmethod unread! ((b buffer) &key (count 1))
+  (setf (buffer-read-ix b)
+	(max 0 (- (buffer-read-ix b) count))))
+
+;;;;;;;;;; Rapids are streams that don't block on peek! or char! operations
 (defclass rapid ()
   ((stream-of :reader stream-of :initarg :stream-of)
    (peeked :accessor peeked :initform (queue))))
@@ -14,8 +39,8 @@
 (defmethod pop-peeked! ((r rapid))
   (pop! (peeked r)))
 
-(defmethod rapid ((s stream))
-  (make-instance 'rapid :stream-of s))
+(defmethod rapid ((s stream) &key (buffer-size 356))
+  (make-instance 'rapid :stream-of s :buffer (make-array buffer-size :element-type 'character)))
 
 (defmethod getc! ((s stream))
   (let ((res (read-char-no-hang s)))
