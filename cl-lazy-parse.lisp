204f457d8a6f452ac503951d57332c0643a2c22c
inaimathi <inaimathi@self>
2015-05-07 00:05:10 -0400

    -Reversible operations with the buffer approach -Simplified some operations (now that stuff is reversible, we don't actually need peek!, which simplifies things. We might not even need rapids to begin with)

--- cl-lazy-parse.lisp
+++ cl-lazy-parse.lisp
@@ -3,51 +3,16 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;; Parsers
 ;;; A parser is a function that might return a result, a +fail+ or a paused state.
-(defmethod run-parser! ((r rapid) (n null)) nil)
-(defmethod run-parser! ((r rapid) (parser function))
+(defmethod run! ((r rapid) (n null)) nil)
+(defmethod run! ((r rapid) (parser function))
   (funcall parser r))
-(defmethod run-parser! ((r rapid) (chr character))
+(defmethod run! ((r rapid) (chr character))
   (funcall (char>> chr) r))
-
-(defmethod run-parser! ((r rapid) (str string))
+(defmethod run! ((r rapid) (str string))
   (funcall (apply #'and>> (map 'list #'char>> str)) r))
-
-(defmethod run! ((r rapid) parser)
-  (result-value (run-parser! r parser)))
-
-;;;;; TODO ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Make parsers recursively reversible, so that you can define the run-parser! string fn as
-;;  (funcall (apply #'and>> (map 'list #'char>> str)) r)
-;; (defmethod run-parser! ((r rapid) (str string))
-;;   (funcall (string->parser str) r))
-
-;; (defun string->parser (str)
-;;   (let ((lst (coerce str 'list)))
-;;     (lambda (r)
-;;       (labels ((cont (v ct)
-;; 		 (cond ((paused-p v) 
-;; 			(pause (cont (resume v) ct)))
-;; 		       ((equal lst v)
-;; 			(coerce (loop repeat (length str) collect (char! r)) 'string))
-;; 		       ((and (> (length str) ct) (alexandria:starts-with-subseq v lst))
-;; 			(cont (peek! r :count (+ ct 1)) (+ ct 1)))
-;; 		       (t +fail+))))
-;; 	(cont (peek! r) 1)))))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defparameter +fail+ (gensym "FAIL"))
 (defun failed? (thing) (eq thing +fail+))
-
-(defstruct result value undo)
-(defmacro result (v &body undo)
-  `(make-result :value ,v :undo (lambda () ,@undo)))
-(defmethod undo! ((res result)) 
-  (funcall (result-undo res)))
-(defun compound (results)
-  (let ((undos (mapcar #'result-undo results)))
-    (result
-	(mapcar #'result-value results)
-      (mapc #'funcall undos))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;; Now then, basic composition
@@ -56,19 +21,22 @@
 If any of them fail, the entire expression fails."
   (lambda (r)
     (let ((acc nil)
+	  (total 0)
 	  (rest parsers))
-      (labels ((next! () (run-parser! r (pop rest)))
-	       (cont (v)
+      (labels ((next! () (multiple-value-call #'cont (run! r (pop rest))))
+	       (cont (v &optional ct)
 		 (cond ((paused-p v)
-			(pause (cont (resume v))))
+			(pause (multiple-value-call #'cont (resume v))))
 		       ((failed? v)
+			(unread! r :count total)
 			+fail+)
-		       ((and rest (result-p v))
+		       (rest
 			(push v acc)
-			(cont (next!)))
+			(incf total ct)
+			(next!))
 		       (t
-			(compound (reverse (cons v acc)))))))
-	(cont (next!))))))
+			(values (reverse (cons v acc)) (+ total ct))))))
+	(next!)))))
 
 (defun or>> (&rest parsers)
   "Takes a list of parsers and matches them in sequence.
@@ -76,54 +44,45 @@ Returns the first successful one.
 If they all fail, the entire expression fails."
   (lambda (r)
     (let ((rest parsers))
-      (labels ((next! () (run-parser! r (pop rest)))
-	       (cont (v)
+      (labels ((next! () (multiple-value-call #'cont (run! r (pop rest))))
+	       (cont (v &optional ct)
 		 (cond ((paused-p v)
-			(pause (cont (resume v))))
+			(pause (multiple-value-call #'cont (resume v))))
 		       ((and rest (failed? v))
-			(cont (next!)))
+			(next!))
 		       ((failed? v)
 			+fail+)
-		       (t v))))
-	(cont (next!))))))
+		       (t (values v ct)))))
+	(next!)))))
 
 (defun many>> (parser)
   "Takes a parser and runs it until it fails.
 Returns the accumulated successes (the empty list, if there were none)."
   (lambda (r)
-    (let ((acc nil))
-      (labels ((next! () (run-parser! r parser))
-	       (cont (v)
+    (let ((acc nil)
+	  (total 0))
+      (labels ((next! () (multiple-value-call #'cont (run! r parser)))
+	       (cont (v &optional ct)
 		 (cond ((paused-p v)
-			(pause (cont (resume v))))
-		       ((failed? v) 
-			(compound (reverse acc)))
+			(pause (multiple-value-call #'cont (resume v))))
+		       ((failed? v)
+			(values (reverse acc) total))
 		       (t
 			(push v acc)
-			(cont (next!))))))
-	(cont (next!))))))
+			(incf total ct)
+			(next!)))))
+	(next!)))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;; Basic transformation
 (defun with (parser fn)
   (lambda (r)
-    (labels ((next! () (run-parser! r parser))
-	     (cont (v)
+    (labels ((cont (v &optional ct)
 	       (cond ((paused-p v)
-		      (pause (cont (resume v))))
+		      (pause (multiple-value-call #'cont (resume v))))
 		     ((failed? v) +fail+)
-		     (t (result (apply fn (result-value v)) (result-undo v))))))
-      (cont (next!)))))
-
-;; (defun on (parser fn)
-;;   (lambda (r)
-;;     (labels ((next! () (run-parser! r parser))
-;; 	     (cont (v)
-;; 	       (cond ((paused-p v)
-;; 		      (pause (cont (resume v))))
-;; 		     ((failed? v) +fail+)
-;; 		     (t (funcall fn v)))))
-;;       (cont (next!)))))
+		     (t (values (apply fn (result-value v)) ct)))))
+      (multiple-value-call #'cont (run! r parser)))))
 
 (defmacro _fn ((&rest args) &body body)
   (multiple-value-bind (final-args ignored)
@@ -137,19 +96,6 @@ Returns the accumulated successes (the empty list, if there were none)."
        (declare (ignore ,@ignored))
        ,@body)))
 
-(defmacro then (parser (&rest args) &body body)
-  (multiple-value-bind (final-args ignored)
-      (loop for a in args
-	 for s = (gensym "IGNORED")
-	 if (eq a '_) 
-	 collect s into res and collect s into vars
-	 else collect a into res
-	 finally (return (values res vars)))
-    `(with ,parser
-	   (lambda ,final-args
-	     (declare (ignore ,@ignored))
-	     ,@body))))
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;; Basic parsers
 (defmethod char>> ((pred function))
@@ -158,11 +104,11 @@ Returns the accumulated successes (the empty list, if there were none)."
 	       (cond ((paused-p v) 
 		      (pause (cont (resume v))))
 		     ((funcall pred v)
-		      (result v (unchar! r v)))
+		      (values v 1))
 		     (t 
 		      (unchar! r v)
 		      +fail+))))
-      (cont (run-parser! r #'char!)))))
+      (cont (run! r #'char!)))))
 
 (defmethod char>> ((pred character))
   (char>> (lambda (c) (eql c pred))))
--- rapid.lisp
+++ rapid.lisp
@@ -5,7 +5,8 @@
 (defun buffer (&key (initial-size 512))
   (make-buffer :arr (make-string initial-size) :size initial-size))
 
-(defmethod more? ((b buffer)) (> (buffer-end-ix b) (buffer-read-ix b)))
+(defmethod more? ((b buffer)) 
+  (and (> (buffer-end-ix b) (buffer-read-ix b)) (> (buffer-end-ix b) 0)))
 
 (defmethod place! ((b buffer) (c character))
   (let ((arr (buffer-arr b)))
@@ -29,56 +30,33 @@
 ;;;;;;;;;; Rapids are streams that don't block on peek! or char! operations
 (defclass rapid ()
   ((stream-of :reader stream-of :initarg :stream-of)
-   (peeked :accessor peeked :initform (queue))))
+   (cached :reader cached :initform (buffer) :initarg :cached)))
 
-(defmethod peeked-ct ((r rapid)) (len (peeked r)))
-
-(defmethod push-peeked! ((r rapid) (c character))
-  (push! c (peeked r)))
-
-(defmethod pop-peeked! ((r rapid))
-  (pop! (peeked r)))
-
-(defmethod rapid ((s stream) &key (buffer-size 356))
-  (make-instance 'rapid :stream-of s :buffer (make-array buffer-size :element-type 'character)))
-
-(defmethod getc! ((s stream))
-  (let ((res (read-char-no-hang s)))
-    (or res (pause (getc! s)))))
+(defmethod rapid ((s stream) &key (buffer-size 256))
+  (make-instance 'rapid :stream-of s :cached (buffer :initial-size buffer-size)))
 
 (defmethod getc! ((r rapid))
-  (getc! (stream-of r)))
-
-(defmethod take ((n integer) (lst list))
-  (loop repeat n for elem in lst collect elem))
-(defmethod take ((n integer) (q queue))
-  (take n (messages q)))
+  (let ((res (read-char-no-hang (stream-of r))))
+    (if res
+	(place! (cached r) res)
+	(pause (getc! r)))))
 
 ;;;;;;;;;; External interface
 ;;; Basic calls
-(defmethod peek! ((r rapid) &key (count 1))
-  (assert (> count 0))
-  (if (>= (peeked-ct r) count)
-      (take count (peeked r))
-      (let ((s (stream-of r)))
-	(labels ((cont (v ct)
-		   (cond ((paused-p v)
-			  (pause (cont (resume v) ct)))
-			 ((= ct 0)
-			  (push-peeked! r v)
-			  (take count (peeked r)))
-			 (t
-			  (push-peeked! r v)
-			  (cont (getc! s) (- ct 1))))))
-	  (cont (getc! s) (- count 1 (peeked-ct r)))))))
-
 (defmethod char! ((r rapid))
-  (cond ((> (peeked-ct r) 0)
-	 (pop-peeked! r))
-	(t (getc! (stream-of r)))))
+  (unless (more? (cached r))
+    (let ((res (getc! r)))
+      (when (paused-p res)
+	(pause (char! r)))))
+  (read! (cached r)))
 
 (defmethod unchar! ((r rapid) (c character))
-  (push-peeked! r c))
+  (declare (ignore c))
+  (unread! (cached r))
+  nil)
+
+(defmethod unread! ((r rapid) &key (count 1))
+  (unread! (cached r) :count count))
 
 ;;; Sugar
 (defmacro with-rapid ((var stream) &body body)
